e09ba384965e23b1f807a4996562351c482d778e
Nick Petersen <86747820+nickpetersen93@users.noreply.github.com>
2024-01-19 15:53:15 -0600

    Update allowing logging individual bouts and better lick detection
    UPDATE: 1/19/2024
    
    We have now included the ability to log the lick data by individual bouts! With this setting turned on, each individual bout will be logged with a line in the file with the side the experimental bottle is on (LEFT or RIGHT), a time stamp, the cage number, which bottle was licked (LEFT or RIGHT), bout lick number, bout lick duration, average lick frequency of that bout, and average inter-lick interval of that bout. Note, this setting will not log the total lick number and lick duration (i.e. licks that do not meet bout criteria).
    
    The "log by bout" setting can be turned on using the GUI under "Parameters to log". This setting can be made the default by changing the Arduino code lines 186-187. For example:
    
    bool log_by_bout = true; 
    bool log_by_time = false;
    //will set the default to "log by bout".
    
    bool log_by_bout = false; 
    bool log_by_time = true;
    //will set the default to "log by time" (original logging method).
    
    We have also made it easier to change the bout cutoff time (the amount of time an animal has to lick 3 times to start the bout) by changing the "bout_cutoff" variable on line 185.
    
    We made some minor changes to the capacitive sensor settings that optimizes the baseline tracking for more consistent lick detection.

--- LIQ_HD_Arduino_Mega/LIQ_HD_Arduino_Mega.ino
+++ LIQ_HD_Arduino_Mega/LIQ_HD_Arduino_Mega.ino
@@ -1,3 +1,46 @@
+/********************************************************
+  LIQ HD
+  Written by Nicholas Petersen
+  https://github.com/nickpetersen93/LIQ_HD
+
+  January, 2023
+
+  This work includes libraries with the following licensing:
+  Adafruit GFX Library - Written by Limor Fried/Ladyada for Adafruit Industries. BSD License
+  Adafruit ILI9341 Library - Written by Limor Fried/Ladyada for Adafruit Industries. MIT license. BSD License.
+  Adafruit FT6206 Library - Written by Limor Fried/Ladyada for Adafruit Industries. MIT license.
+  Adafruit MPR121 Library - Written by Limor Fried/Ladyada for Adafruit Industries. MIT license. BSD License.
+  RTCLib Library - Written by JeeLabs. MIT license.
+    
+  This work is released under This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+  https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+
+  Copyright (c) 2023 Nicholas Petersen. Creative Commons BY-NC-SA License. Non-Commercial Use Only. 
+  Adapter’s License must be a Creative Commons BY-NC-SA or Compatible License. 
+  All text above must be included in any redistribution. See LICENSE.md for detailed information.
+
+  UNLESS OTHERWISE SEPARATELY UNDERTAKEN BY THE LICENSOR, TO THE EXTENT POSSIBLE, THE LICENSOR OFFERS 
+  THE LICENSED MATERIAL AS-IS AND AS-AVAILABLE, AND MAKES NO REPRESENTATIONS OR WARRANTIES OF ANY KIND 
+  CONCERNING THE LICENSED MATERIAL, WHETHER EXPRESS, IMPLIED, STATUTORY, OR OTHER. THIS INCLUDES, 
+  WITHOUT LIMITATION, WARRANTIES OF TITLE, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
+  NON-INFRINGEMENT, ABSENCE OF LATENT OR OTHER DEFECTS, ACCURACY, OR THE PRESENCE OR ABSENCE OF ERRORS, 
+  WHETHER OR NOT KNOWN OR DISCOVERABLE. WHERE DISCLAIMERS OF WARRANTIES ARE NOT ALLOWED IN FULL OR IN 
+  PART, THIS DISCLAIMER MAY NOT APPLY TO YOU.
+  
+  TO THE EXTENT POSSIBLE, IN NO EVENT WILL THE LICENSOR BE LIABLE TO YOU ON ANY LEGAL THEORY (INCLUDING, 
+  WITHOUT LIMITATION, NEGLIGENCE) OR OTHERWISE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL, 
+  CONSEQUENTIAL, PUNITIVE, EXEMPLARY, OR OTHER LOSSES, COSTS, EXPENSES, OR DAMAGES ARISING OUT OF THIS 
+  PUBLIC LICENSE OR USE OF THE LICENSED MATERIAL, EVEN IF THE LICENSOR HAS BEEN ADVISED OF THE 
+  POSSIBILITY OF SUCH LOSSES, COSTS, EXPENSES, OR DAMAGES. WHERE A LIMITATION OF LIABILITY IS NOT 
+  ALLOWED IN FULL OR IN PART, THIS LIMITATION MAY NOT APPLY TO YOU.
+  
+  THE DISCLAIMER OF WARRANTIES AND LIMITATION OF LIABILITY PROVIDED ABOVE SHALL BE INTERPRETED IN A 
+  MANNER THAT, TO THE EXTENT POSSIBLE, MOST CLOSELY APPROXIMATES AN ABSOLUTE DISCLAIMER AND WAIVER OF 
+  ALL LIABILITY.
+
+
+********************************************************/
+
 #include <Adafruit_GFX.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -19,9 +62,9 @@
 //#define DEBUG //uncomment to turn on debug mode, prints info to Serial Monitor
 
 #ifdef DEBUG
-#define DEBUG_PRINT(x)     Serial.print (x)
-#define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
-#define DEBUG_PRINTLN(x)  Serial.println (x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTDEC(x) Serial.print(x, DEC)
+#define DEBUG_PRINTLN(x) Serial.println(x)
 #else
 #define DEBUG_PRINT(x)
 #define DEBUG_PRINTDEC(x)
@@ -36,8 +79,8 @@ Adafruit_FT6206 ts = Adafruit_FT6206();
 #define TFT_DC 9
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-RTC_PCF8523 rtc; // define the Real Time Clock object
-const int chipSelect = 7; // for the data logging shield, changed from digital pin 10
+RTC_PCF8523 rtc;           // define the Real Time Clock object
+const int chipSelect = 7;  // for the data logging shield, changed from digital pin 10
 #define redLEDpin 13
 DateTime now;
 
@@ -59,7 +102,7 @@ uint16_t currtouched2 = 0;
 uint16_t lasttouched3 = 0;
 uint16_t currtouched3 = 0;
 
-File logfile; // the logging file
+File logfile;  // the logging file
 char filename[] = "00000000.CSV";
 
 TS_Point p;
@@ -67,6 +110,7 @@ extern uint8_t welcome_logo[];
 extern uint8_t main_logo[];
 extern uint8_t settings_icon[];
 extern uint8_t settings_icon2[];
+unsigned long startmillis;
 String E_side = "RIGHT";
 String display_page = "main";
 bool show_time = true;
@@ -94,13 +138,15 @@ int default_auto_cal_time;
 int default_auto_cal_sec_since_last_lick;
 bool auto_cal_flag = true;
 unsigned long cal_timer;
-String logging[6];
+String logging[8];
 bool log_LN = true;
 bool log_LD = true;
 bool log_BN = true;
 bool log_BD = true;
 bool log_BLN = true;
 bool log_BLD = true;
+bool recording = false;
+String licked_bottle;
 
 //enough for 18 cages, 2 lickometers per cage
 unsigned long LickDuration[36];
@@ -109,6 +155,8 @@ unsigned long BoutNumber[36];
 unsigned long BoutDuration[36];
 unsigned long BoutLickNumber[36];
 unsigned long BoutLickDuration[36];
+float LickFrequency[36];
+float ILI[36];
 unsigned long Elapsedtime[36];
 unsigned long total_LN[36];
 int lick_bout_countdown[36];
@@ -117,43 +165,49 @@ unsigned long bout_start_timer[36];
 bool in_bout[36];
 bool licking[36];
 unsigned long time_now[36];
+unsigned long last_lick_time[36];
 
 // how many minutes between grabbing data and logging it.
-int LOG_INTERVAL = 1; // IN MINUTES! between entries (reduce to take more/faster data)
+int LOG_INTERVAL = 1;  // IN MINUTES! between entries (reduce to take more/faster data)
 // how many milliseconds before writing the logged data permanently to disk
-int SYNC_INTERVAL = 10; // IN MINUTES! between calls to flush() - to write data to the card
-unsigned long emergency_counter = 60050; //1 minute +50ms
+int SYNC_INTERVAL = 10;                   // IN MINUTES! between calls to flush() - to write data to the card
+unsigned long emergency_counter = 60050;  //1 minute +50ms
 
 // Typically touch threshold > release threshold for debouncing
 // Decrease value to increase sensitivity
-int touch_threshold = 2; //default = 12
-int release_threshold = 0; //default = 6
-bool auto_cal = false;
-int auto_cal_time = 7;
-int auto_cal_sec_since_last_lick = 60;
+int touch_threshold = 2;    //default = 12
+int release_threshold = 0;  //default = 6
+//int current = 50;           //Range of 1-63uA //ignore for now
+int debounce = 0;           //range 0-7
+bool auto_cal = true;       //flag to turn on daily auto calibration by defaults
+int auto_cal_time = 7;      //hour in the day when auto cal will occur (best in light cycle when animals less active)
+int auto_cal_sec_since_last_lick = 60;  //Time in seconds of inactivity before auto cal will happen
+int bout_cutoff = 3000;     //bout cut off time in milliseconds 
+bool log_by_bout = false;    //flag to turn on "log by bout" mode by default, ONLY ONE SHOULD BE TRUE
+bool log_by_time = true;   //flag to turn on "log by time" mode by default, ONLY ONE SHOULD BE TRUE
 
-int lights_on = 6; //time of day (24hr time) housing lights turn ON
-int lights_off = 18; //time of day (24hr time) houseing lights turn OFF
+int lights_on = 6;    //time of day (24hr time) housing lights turn ON
+int lights_off = 18;  //time of day (24hr time) houseing lights turn OFF
 
 //==========================================================================================
 
 void setup() {
-
+#ifdef DEBUG
   Serial.begin(115200);
+#endif
 
-  pinMode(5, OUTPUT); // set screen brightness pin as output
+  pinMode(5, OUTPUT);  // set screen brightness pin as output
   analogWrite(5, 255);
 
-  set_defaults(); //save coded settings as the default settings
+  set_defaults();  //save coded settings as the default settings
 
-  start_display(); //initialize display
+  start_display();  //initialize display
 
-  start_rtc(); //iniitialize rtc
+  start_rtc();  //iniitialize rtc
 
-  WelcomeScreen(); //display welcome screen at startup
+  WelcomeScreen();  //display welcome screen at startup
 
-  set_brightness(); //set screen brightness depending on light cycle
-
+  set_brightness();  //set screen brightness depending on light cycle
 }
 
 //==========================================================================================
@@ -169,7 +223,6 @@ void loop() {
   SD_error_page();
 
   settings_page();
-  
-  error_page();
 
+  error_page();
 }
